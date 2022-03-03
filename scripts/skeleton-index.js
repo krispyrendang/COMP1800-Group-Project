5b2dca476d61ba7a3348f44271d84f69e3f55105
krispyrendang <taswinder.s@gmail.com>
2022-03-03 15:03:44 -0800

    added separate nav bar for index and login page. Added option for organizers to redirect to own login page. Added separate landing page for organizers/volunteer leaders. 2nd login page and landing page is NOT linked yet.

---
+++ scripts/skeleton-index.js
@@ -0,0 +1,9 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeletonIndex(){
+    console.log($('#navbar').load('./text/nav-index.html'));
+    console.log($('#footer').load('./text/footer.html'));
+}
+loadSkeletonIndex();  //invoke the function
